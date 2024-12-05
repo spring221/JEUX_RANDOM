943b73f19cc9ab263fcf695e4b100def017dce8f
spring221 <tallaspring24@gmail.com>
2024-12-05 14:07:04 -0500

    utilisation fonction rand fin

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int arc,char *arvg[]){
 
-    int i;
+/*avec rand() pour un intervalle donner
+1-initialiser le strand(seed) why?
+2-utiliser le %(N+1) {[0,N]} 
+3- +1 Pour que N sois parmi les valeurs*/
+  
+    srand(time(NULL));
+    int Rand_nbre = rand() % (100) + 1;
 
-    for (i = 0; i < 3; i++)
-        printf("%d\n", rand());
+    printf("%d\n", Rand_nbre);
 
     return 0;
 }
