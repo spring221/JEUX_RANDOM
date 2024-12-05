4546b6df105fcc8b6a4efab96edc6c58bd738c85
spring221 <tallaspring24@gmail.com>
2024-12-05 14:30:56 -0500

    premier version

--- main.c
+++ main.c
@@ -11,6 +11,41 @@ int main(int arc,char *arvg[]){
   
     srand(time(NULL));
     int Rand_nbre = rand() % (100) + 1;
+    int nbre_utilisateur ;
+    int trust = 0;
+
+    do{
+
+        printf("veiller deviner un nombre entre 1 et 100\n");
+        scanf("%d",&nbre_utilisateur);
+
+
+        if(Rand_nbre == nbre_utilisateur){
+
+            trust =1;
+
+            printf("felicitation vous avez trouvez le nombre mysterieux");
+
+            
+        }
+        else{
+            if(Rand_nbre > nbre_utilisateur){
+
+                printf("nombre mysterieux superieux au nombre entrer\n");
+
+
+            }
+            else{
+
+                printf("nombre mysterieux inferieur au nombre entrer\n");
+
+
+            }
+        }
+
+
+
+    }while(trust == 0);
 
     printf("%d\n", Rand_nbre);
 
